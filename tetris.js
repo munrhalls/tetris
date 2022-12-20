a814f532996f3ab65f3edf83d75baf9a10f0eb2a
John <antarcticdepths@gmail.com>
2022-12-21 00:58:02 +0100

    random dom elem moving top to bot!

--- tetris.js
+++ tetris.js
@@ -1,3 +1,35 @@
-const tetris = document.getElementById("tetris");
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (
+      /* function FrameRequestCallback */ callback,
+      /* DOMElement Element */ element
+    ) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
 
-console.log(tetris);
+var elem = document.getElementById("tetris");
+var startTime = undefined;
+
+function render(time) {
+  if (time === undefined) time = Date.now();
+  if (startTime === undefined) startTime = time;
+
+  elem.style.height = "100px";
+  elem.style.background = "black";
+  elem.style.top = (((time - startTime) / 5) % 500) + "px";
+}
+
+elem.onclick = function () {
+  (function animloop() {
+    render();
+    requestAnimFrame(animloop, elem);
+  })();
+};
