cb0a6ae2b314cc63496192804d0cc960a7916ee0
John <antarcticdepths@gmail.com>
2023-01-05 22:05:15 +0100

    TESTING

--- gameHandlers/scorer.js
+++ gameHandlers/scorer.js
@@ -3,52 +3,9 @@ export const scorer = {
   getLineClears: function () {
     let lineClearsList = [];
 
-    // const rows = [...document.getElementsByClassName("row")];
-    const testRows = [...document.getElementsByClassName("row")];
-    let count = 3;
+    const rows = [...document.getElementsByClassName("row")];
 
-    // for (let row of rows) {
-    for (let row of testRows) {
-      count++;
-      if (count > 15) break;
-      const line = [...row.children];
-      let isLineBreak = true;
-      for (let cell of line) {
-        cell.classList.add("frozen");
-        cell.classList.add("orange");
-        console.log(cell);
-        if (![...cell.classList].includes("frozen"))
-          return (isLineBreak = false);
-      }
-
-      if (isLineBreak) lineClearsList.push(line);
-    }
-
-    return lineClearsList;
-  },
-  handleLineClears: function handleLineClears() {
-    let lineClearsList = this.getLineClears();
-    let accumulator = 0;
-    let isNeighbour = false;
-
-    for (let i = 0; i < lineClearsList.length; i++) {
-      console.log(lineClearsList[i]);
-    }
-
-    console.log(lineClears);
-  },
-  isNonZeroClears: function isNonZeroClears() {
-    let isOverZero = false;
-    const rndNums = [4, 5, 6, 8, 9, 12];
-    const testRows = [...document.getElementsByClassName("row")];
-    rndNums.forEach((num) => {
-      let line = [...testRows[num].children];
-      line.forEach((line) => {
-        line.classList.add("orange");
-      });
-    });
-
-    for (let row of testRows) {
+    for (let row of rows) {
       const line = [...row.children];
       let isLineBreak = true;
 
@@ -58,31 +15,45 @@ export const scorer = {
           break;
         }
       }
-      console.log(isLineBreak);
+
+      if (isLineBreak) {
+        line.num = lineClearsList.indexOf(line);
+        lineClearsList.push(line);
+      }
+    }
+
+    return lineClearsList;
+  },
+  handleLineClears: function handleLineClears() {
+    let lineClearsList = this.getLineClears();
+    console.log(lineClearsList);
+    let accumulator = 0;
+    let isNeighbour = false;
+
+    for (let i = 0; i < lineClearsList.length; i++) {
+      console.log(lineClearsList[i]);
+    }
+  },
+  isNonZeroClears: function isNonZeroClears() {
+    let isOverZero = false;
+    const rows = [...document.getElementsByClassName("row")];
+
+    for (let row of rows) {
+      const line = [...row.children];
+      let isLineBreak = true;
+
+      for (let cell of line) {
+        if (![...cell.classList].includes("frozen")) {
+          isLineBreak = false;
+          break;
+        }
+      }
+
       if (isLineBreak) {
         isOverZero = true;
         break;
       }
     }
-    // const rows = [...document.getElementsByClassName("row")];
-
-    // for (let row of rows) {
-    //   const line = [...row.children];
-    //   let isLineBreak = true;
-
-    //   for (let cell of line) {
-    //     if (![...cell.classList].includes("frozen")) {
-    //       isLineBreak = false;
-    //       break;
-    //     }
-    //   }
-    //   console.log(isLineBreak);
-    //   if (isLineBreak) {
-    //     isOverZero = true;
-    //     break;
-    //   }
-    // }
-
     return isOverZero;
   },
   handleScoring: function () {
@@ -91,3 +62,19 @@ export const scorer = {
     displayer.innerText = `${this.score}`;
   },
 };
+
+function setUpScorerTests() {
+  const rndNums = [4, 5, 6, 8, 9, 12];
+  const testRows = [...document.getElementsByClassName("row")];
+  rndNums.forEach((num) => {
+    let line = testRows[num].children;
+    [...line].forEach((cell) => {
+      cell.classList.add("orange");
+      cell.classList.add("frozen");
+    });
+  });
+}
+
+setTimeout(() => {
+  setUpScorerTests();
+}, 1000);
