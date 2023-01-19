d21768ec4a70da4f202b696e9964d97553d8db46
John <antarcticdepths@gmail.com>
2023-01-19 17:22:43 +0100

    game

--- 1_building/makeGameInterface.js
+++ 1_building/makeGameInterface.js
@@ -4,9 +4,13 @@ export default function makeGameInterface() {
   const startBtn = document.getElementById("start");
   const pauseBtn = document.getElementById("pause");
   startBtn.onclick = function () {
+    startBtn.classList.add("hidden");
+    pauseBtn.classList.remove("hidden");
     runner.runGame();
   };
   pauseBtn.onclick = function () {
     runner.stopGame();
+    startBtn.classList.remove("hidden");
+    pauseBtn.classList.add("hidden");
   };
 }
